f494423aba874a2c2b0a63853df413811185c126
shizukumurasakionelovesogood <shizukumurasakionelove@gmail.com>
2025-04-03 22:12:05 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
